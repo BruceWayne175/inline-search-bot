f034a3880c27ef00ff2da4668ba32907d1b4710d
BruceWayne175 <75173826+BruceWayne175@users.noreply.github.com>
2020-12-04 08:54:29 +0530

    Add files via upload

---
+++ info.py
@@ -0,0 +1,31 @@
+import re
+from os import environ
+
+# Bot information
+SESSION = environ.get('SESSION', 'Media_search')
+USER_SESSION = environ.get('USER_SESSION', 'User_Bot')
+API_ID = int(environ['API_ID'])
+API_HASH = environ['API_HASH']
+BOT_TOKEN = environ['BOT_TOKEN']
+
+# Bot settings
+MAX_RESULTS = int(environ.get('MAX_RESULTS', 10))
+CACHE_TIME = int(environ.get('CACHE_TIME', 300))
+
+# Admins & Channels
+ADMINS = [int(admin) if re.search('^\d+$', admin) else admin for admin in environ['ADMINS'].split()]
+CHANNELS = [int(channel) if re.search('^.\d+$', channel) else channel for channel in environ['CHANNELS'].split()]
+
+# MongoDB information
+DATABASE_URI = environ['DATABASE_URI']
+DATABASE_NAME = environ['DATABASE_NAME']
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+
+# Messages
+START_MSG = """
+**Hi, I'm Media Search bot**
+
+Here you can search files in inline mode. Just press following buttons and start searching.
+"""
+
+SHARE_BUTTON_TEXT = 'Checkout {username} for searching files'
